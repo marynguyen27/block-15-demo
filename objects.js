73e54c883877c8f4d081dc2660cfeb53f9087baa
Mary Nguyen <marynguyen27@gmail.com>
2024-05-22 19:05:33 -0700

    object examples bracket and dot notations

--- objects.js
+++ objects.js
@@ -13,3 +13,22 @@ const shoe = {
 };
 
 console.log(shoe);
+
+// So how do you ACCESS the values in the object?
+// In arrays, you use indexing (the square brackets)
+// Accessing values in objects:
+// 2 main ways (these two give the same results)
+// 1) BRACKET NOTATION (similar to arrays)
+let brand = shoe['brand'];
+console.log('Brand:', brand);
+// 2) DOT NOTATION *this way is more common & popular*
+brand = shoe.brand;
+console.log('Brand:', brand);
+console.log('Price:', shoe.price);
+// what if you don't know the key in advance
+// what if the key is a variable that comes from the user?
+let userKey = prompt('Type in a key:');
+console.log('Using userKey:', shoe[userKey]); // looks for a key whatever the prompt is
+console.log('Using userKey:', shoe.userKey); // looks for a key called "userKey"
+// Bracket notiation is used for VARIABLES
+// Otherwise, dot notation is great
