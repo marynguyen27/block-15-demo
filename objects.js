a2b7350fcb6cdffa62856104ef74024d6e327eaf
Mary Nguyen <marynguyen27@gmail.com>
2024-05-22 18:50:49 -0700

    example object shoe

---
+++ objects.js
@@ -0,0 +1,15 @@
+// OBJECTS
+
+const shoe = {
+  brand: 'Converse',
+  hasLaces: true,
+  hasVelcro: false,
+  color: {
+    primary: 'green',
+    secondary: 'white',
+  },
+  price: 65.0,
+  activity: ['walking', 'skateboarding', 'basketball'],
+};
+
+console.log(shoe);
