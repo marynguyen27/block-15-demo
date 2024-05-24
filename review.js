197bd3810d3860d2b9d66d391d140fea3cb9f8ba
Mary Nguyen <marynguyen27@gmail.com>
2024-05-23 18:23:02 -0700

    review letter counter

---
+++ review.js
@@ -0,0 +1,43 @@
+/* 
+PROMPT: Given a random sentence,
+make an object that counts every letter of that sentence
+
+"Hello"
+
+{
+    'H': 1
+    'E': 1
+    'L': 2
+    'O': 1
+}
+
+*/
+
+let inputStr = 'hello world';
+
+function stringCounter(inputStr) {
+  const strArray = inputStr.split('');
+  console.log('Array:', strArray);
+  let answer = {};
+  for (let i = 0; i < strArray.length; i++) {
+    let letter = strArray[i];
+
+    if (letter === ' ') {
+      continue; // continue the loop at the next value of i (skip the space, only counts letters)
+      // break; // ends the loop right here (counts things before the space)
+    }
+
+    // is this letter already in the object?
+    // if it's new, we add it to the object
+    if ((!Object.hasOwn(answer), letter)) {
+      answer[letter] = 1; // because we're using a variable, use the brackets
+    } else {
+      // if it's not new, then we update the letter value
+      answer[letter] += 1;
+    }
+  }
+
+  return answer;
+}
+
+console.log('Answer:', stringCounter(inputStr));
